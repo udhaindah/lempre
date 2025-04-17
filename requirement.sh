a75454d73f07d74073e6ceea3b51e458c6dc4d3b
udhaindah <udhaindah937@gmail.com>
2025-04-17 11:43:41 +0700

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##Install Requirements
+pip install --upgrade requests
+pip install --upgrade asyncio
+pip install --upgrade loguru
+pip install --upgrade websockets_proxy
+pip install --upgrade fake_useragent
+pip install --upgrade random_user_agent
+pip install --upgrade requests 
+pip install --upgrade pyfiglet
+pip install --upgrade cloudscraper
+pip install --upgrade colorama
+pip install ua-parser-py
+pip install -U 'requests[socks]'
